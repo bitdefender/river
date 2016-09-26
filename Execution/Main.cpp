bc08846495176e7ea25c0e7b895a4f52744c283e
Alexandra Sandulescu <asandulescu@bitdefender.com>
2016-09-26 18:52:47 +0300

    [Execution] Add proper ifdefs for Win library

--- Execution/Main.cpp
+++ Execution/Main.cpp
@@ -1,4 +1,6 @@
+#ifdef _WIN32
 #include <Windows.h>
+#endif
 
 #include "ExternExecutionController.h"
 #include "InprocessExecutionController.h"
@@ -19,6 +21,8 @@ DLL_PUBLIC_EXECUTION void DeleteExecutionController(ExecutionController *ptr) {
 }
 
 
+#ifdef _WIN32
 BOOL WINAPI DllMain(_In_ HINSTANCE hinstDLL, _In_ DWORD fdwReason, _In_ LPVOID lpvReserved) {
 	return TRUE;
 }
+#endif
