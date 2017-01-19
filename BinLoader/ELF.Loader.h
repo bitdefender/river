75bf6d3517c2226e375f08781d36e41145664951
Alexandra Sandulescu <asandulescu@bitdefender.com>
2017-01-19 12:11:31 +0200

    [BinLoader] Add parse verdef functionality
    ** Parse SHT_VERDEF
    ** Call DT_INIT function after mapping library
    ** Fix solve_path issue for char * paths
    ** Credits to Doru

--- BinLoader/ELF.Loader.h
+++ BinLoader/ELF.Loader.h
@@ -77,15 +77,26 @@ namespace ldr {
 	};
 
 
-	class ELFSymbolVersioning {
+	class ELFSymbolVersionNeeded {
 	public:
 		DWORD index;
 		std::string version;
 		std::string module;
 
-		ELFSymbolVersioning(DWORD idx, std::string v, std::string m) :
+		ELFSymbolVersionNeeded(DWORD idx, std::string v, std::string m) :
 			index(idx), version(v), module(m) {}
 	};
+
+	class ELFSymbolVersionDefined {
+	public :
+		WORD index;
+		std::string version;
+
+		ELFSymbolVersionDefined(WORD idx, std::string v) :
+			index(idx), version(v)
+		{}
+	};
+
 
 	class ELFSection {
 	public:
@@ -93,8 +104,11 @@ namespace ldr {
 		unsigned char *data;
 
 		ELFSection *versions;
-		std::vector<ELFSymbolVersioning> sVers;
-		std::vector<ELFSymbolVersioning *> idxSVers;
+		std::vector<ELFSymbolVersionNeeded> snVers;
+		std::vector<ELFSymbolVersionNeeded *> idxSnVers;
+
+		std::vector<ELFSymbolVersionDefined> sdVers;
+		std::vector<ELFSymbolVersionDefined *> idxSdVers;
 
 		ELFSection();
 		bool Load(FILE *fModule);
@@ -132,6 +146,7 @@ namespace ldr {
 
 		ELFSection *names;
 		ELFSection *gnu_versions_r;
+		ELFSection *gnu_versions_d;
 
 		bool isValid;
 
@@ -144,6 +159,7 @@ namespace ldr {
 		bool RelocateSection(void *r, DWORD count, const ELFSection &symb, const ELFSection &names, DWORD offset);
 
 		bool ParseVerNeed(ELFSection &s);
+		bool ParseVerDef(ELFSection &s);
 		bool ParseDynamic(const ELFSection &section);
 
 		bool Relocate(DWORD newBase);
@@ -163,6 +179,9 @@ namespace ldr {
 
 		virtual bool Map(AbstractMapper &mapr, AbstractImporter &impr, DWORD &baseAddr);
 		virtual bool GetExport(const char *funcName, DWORD &funcRVA) const;
+
+
+		virtual void ForAllExports(std::function<void(const char *, const DWORD, const char *, const DWORD, const unsigned char *)> verb) const;
 
 		virtual bool IsValid() const {
 			return isValid;
--- BinLoader/Inproc.Native.Importer.h
+++ BinLoader/Inproc.Native.Importer.h
@@ -2,9 +2,38 @@
 #define _INPROC_NATIVE_IMPORTER_H_
 
 #include "Abstract.Importer.h"
+#include <vector>
+#include <string>
 
 namespace ldr {
 	class InprocNativeImporter : public AbstractImporter {
+
+	private:
+		struct CachedEntry {
+			std::string funcName;
+			unsigned int ordinal;
+			std::string version;
+			DWORD rva;
+		};
+
+		struct CachedModule {
+			std::string moduleName;
+			std::vector<CachedEntry> entries;
+
+			void AddImport(const char *funcName, const unsigned int ordinal, const char *version, DWORD rva);
+
+			DWORD FindCachedImport(const char *funcName);
+			DWORD FindCachedImport(const unsigned int funcOrdinal);
+			DWORD FindCachedImport(const char *funcName, const char *version);
+		};
+		std::vector<CachedModule> modules;
+
+
+		CachedModule *AddModule(const char *name);
+		CachedModule *FindModule(const char *moduleName);
+
+		CachedModule *GetModule(const char *name);
+
 	public:
 		virtual DWORD FindImport(const char *moduleName, const char *funcName);
 		virtual DWORD FindImport(const char *moduleName, const unsigned int funcOrdinal);
