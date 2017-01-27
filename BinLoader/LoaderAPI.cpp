1bbde6747ed2a64ba4e7694d917dd8a606c95f47
Teodor STOENESCU <tstoenescu@bitdefender.com>
2017-01-27 17:11:04 +0200

    [*] Minor fixes introduced by the rebase.

--- BinLoader/LoaderAPI.cpp
+++ BinLoader/LoaderAPI.cpp
@@ -39,7 +39,7 @@ void CreateModule(const char *libname, ldr::AbstractBinary *&module) {
 	module = fExec;
 }
 
-void MapModule(MODULE_PTR &module, BASE_PTR &baseAddr,
+void MapModule(ldr::AbstractBinary *&module, BASE_PTR &baseAddr,
 		bool callConstructors, int shmFd, unsigned long offset) {
 	ldr::ShmMapper mpr(shmFd, offset);
 	ldr::InprocNativeImporter imp;
