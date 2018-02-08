e8ce9865e59acf91b52f28bf4e3c071ff106ec58
Alexandra Sandulescu <asandulescu@bitdefender.com>
2018-02-08 16:08:13 +0200

    revtracer: rep translation works for all tracers

--- revtracer/RelocableCodeBuffer.cpp
+++ revtracer/RelocableCodeBuffer.cpp
@@ -39,14 +39,27 @@ void RelocableCodeBuffer::CopyToFixed(nodep::BYTE *dst) const {
  }
 
 void RelocableCodeBuffer::MarkRepFini() {
-	nodep::DWORD actualCodeSize = cursor - repInitCursor - 17 /*3 * jmp imm32 + loop imm8*/;
+	const nodep::DWORD jmpSize = 5;
+	const nodep::DWORD loopSize = 2;
+	const nodep::DWORD repinitInstrSize = jmpSize + jmpSize + loopSize + jmpSize;
+	const nodep::DWORD farloopInstrSize = jmpSize + 3 + loopSize;
+	const nodep::DWORD repfiniInstrSize = jmpSize + jmpSize + farloopInstrSize;
+
+	nodep::DWORD actualCodeSize = cursor - repInitCursor - (repinitInstrSize + repfiniInstrSize);
 
 	// fix jumps
-	nodep::BYTE *jmpRepFiniImm = repInitCursor + 5 /*jmp imm32*/ + 2/*loop imm8*/ + 1; /*jmp imm32*/
-	*(nodep::DWORD *)(jmpRepFiniImm) = actualCodeSize + 5 /*jmp loop*/;
+	nodep::BYTE *jmpWrapin = repInitCursor + 1;
+	*(nodep::DWORD *)(jmpWrapin) += actualCodeSize;
 
-	nodep::BYTE *jmpLoopImm = repInitCursor + 5 /*jmp imm32*/ + 2 /*loop imm8*/ + 5 /*jmp imm32*/ + actualCodeSize + 1;
-	*(nodep::DWORD *)(jmpLoopImm) = -1 * (5 /*jmp imm32*/ + actualCodeSize + 5 /*repinit*/ + 2 /*loop imm8*/);
+	nodep::BYTE *jmpCodeout = repInitCursor + jmpSize + jmpSize + loopSize + 1;
+	*(nodep::DWORD *)(jmpCodeout) += actualCodeSize;
+
+	nodep::BYTE *jmpLoop = repInitCursor + jmpSize + jmpSize + loopSize + jmpSize + actualCodeSize + 1;
+	*(nodep::DWORD *)(jmpLoop) -= actualCodeSize;
+
+	nodep::BYTE *jmpInit = cursor - (loopSize + 3 + jmpSize) + 1;
+	*(nodep::DWORD *)(jmpInit) -= actualCodeSize;
+
 	needsRepFix = false;
 	repInitCursor = NULL;
 }
--- revtracer/RiverRepAssembler.cpp
+++ revtracer/RiverRepAssembler.cpp
@@ -1,61 +1,90 @@
 #include "RiverRepAssembler.h"
 
-void AssembleJmpInstruction(RelocableCodeBuffer &px86, nodep::DWORD &instrCounter) {
-	static const nodep::BYTE jmpCode[] = {
-		0xE9, 0x00, 0x00, 0x00, 0x00
-	};
-	rev_memcpy(px86.cursor, jmpCode, sizeof(jmpCode));
-	px86.cursor += sizeof(jmpCode);
-	instrCounter += 1;
-}
+const unsigned jmpSize = 5;
+const unsigned loopSize = 2;
 
-void AssembleKnownJmpInstruction(const RiverInstruction &ri, RelocableCodeBuffer &px86, nodep::DWORD &instrCounter) {
-	static const nodep::BYTE jmpCode[] = {
-		0xE9, 0x00, 0x00, 0x00, 0x00
-	};
-	int addrJump = 2/*loop init*/ + 5 /*jmp repfini*/;
-
-	rev_memcpy(px86.cursor, jmpCode, sizeof(jmpCode));
-
-	*(unsigned int *)(&(px86.cursor[0x1])) = addrJump;
-
-	px86.cursor += sizeof(jmpCode);
-	instrCounter += 1;
-}
-
-void AssembleLoopInstruction(const RiverInstruction &ri, RelocableCodeBuffer &px86, nodep::DWORD &instrCounter) {
-	static const nodep::BYTE loopCode[] = {
-		0x00, 0x00
-	};
-
-	nodep::BYTE addrLoop = -1 * (2 /*loop init*/ + 5 /*jmp code*/);
-
-	nodep::BYTE opcode = 0x0;
-	if (ri.modifiers & RIVER_MODIFIER_REP) {
-		opcode = 0xE2;
-	} else if (ri.modifiers & RIVER_MODIFIER_REPZ) {
-		opcode = 0xE1;
-	} else if (ri.modifiers & RIVER_MODIFIER_REPNZ) {
-		opcode = 0xE0;
-	} else {
-		DEBUG_BREAK;
-	}
-
-	rev_memcpy(px86.cursor, loopCode, sizeof(loopCode));
-
-	*(nodep::BYTE *)(px86.cursor) = opcode;
-	*(nodep::BYTE *)(&px86.cursor[1]) = addrLoop;
-
-	px86.cursor += sizeof(loopCode);
-	instrCounter += 1;
-}
-
-void AssembleDebugBreak(const RiverInstruction &ri, RelocableCodeBuffer &px86, nodep::DWORD &instrCounter) {
+void AssembleDebugBreak(RelocableCodeBuffer &px86, nodep::DWORD &instrCounter) {
 	static const nodep::BYTE intCode = 0xcc;
 
 	*px86.cursor = intCode;
 	px86.cursor += 1;
 	instrCounter += 1;
+}
+
+void AssembleRepinitInstruction(const RiverInstruction &ri, RelocableCodeBuffer &px86, nodep::DWORD &instrCounter) {
+	static const nodep::BYTE repinitCode[] = {
+		0xE9, 0x00, 0x00, 0x00, 0x00,        //jmp wrapin
+		0xE9, 0x00, 0x00, 0x00, 0x00,        //jmp codein
+		0x00, 0x00,                          //loop init
+		0xE9, 0x00, 0x00, 0x00, 0x00		 //jmp codeout
+	};
+
+	rev_memcpy(px86.cursor, repinitCode, sizeof(repinitCode));
+
+	unsigned wrapinOffset = jmpSize + loopSize + jmpSize +
+		jmpSize + jmpSize + jmpSize; // actual code size is fixed RelocableCodeBuffer::MarkRepFini
+	*(unsigned int *)(px86.cursor + 1) = wrapinOffset;
+
+	unsigned codeinOffset = loopSize + jmpSize;
+	*(unsigned int *)(px86.cursor + 6) = codeinOffset;
+
+	nodep::BYTE loopOpcode = 0x0;
+	if (ri.modifiers & RIVER_MODIFIER_REP) {
+		loopOpcode = 0xE2;
+	} else if (ri.modifiers & RIVER_MODIFIER_REPZ) {
+		loopOpcode = 0xE1;
+	} else if (ri.modifiers & RIVER_MODIFIER_REPNZ) {
+		loopOpcode = 0xE0;
+	} else {
+		DEBUG_BREAK;
+	}
+	*(px86.cursor + 10) = loopOpcode;
+	*(px86.cursor + 11) = (nodep::BYTE)(-1 * (loopSize + jmpSize));
+
+	*(unsigned int *)(px86.cursor + 13) = jmpSize; //actual code size is fixed in RelocableCodeBuffer::MarkRepFini
+
+	px86.cursor += sizeof(repinitCode);
+	instrCounter += 4;
+}
+
+void AssembleFarloopInstruction(RelocableCodeBuffer &px86, nodep::DWORD &instrCounter) {
+	static const nodep::BYTE farloopCode[] = {
+		0xE9, 0x00, 0x00, 0x00, 0x00,	//jmp init
+		0x8D, 0x49, 0x01,				//lea ecx, [ecx + 1]
+		0xE2, 0x00						//loop farloop
+	};
+
+	rev_memcpy(px86.cursor, farloopCode, sizeof(farloopCode));
+
+	unsigned initOffset = -1 * (jmpSize + jmpSize + jmpSize + jmpSize +
+			loopSize + jmpSize); //actual code size is fixed by RelocableCodeBuffer::MarkRepFini
+	*(nodep::DWORD *)(px86.cursor + 1) = initOffset;
+
+	nodep::BYTE loopOffset = (nodep::BYTE)(-1 * (loopSize + 3 + jmpSize));
+	*(px86.cursor + 9) = loopOffset;
+
+	px86.cursor += sizeof(farloopCode);
+	instrCounter += 3;
+}
+
+void AssembleRepfiniInstruction(RelocableCodeBuffer &px86, nodep::DWORD &instrCounter) {
+	static const nodep::BYTE repfiniCode[] = {
+		0xE9, 0x00, 0x00, 0x00, 0x00, //jmp loop
+		0xE9, 0x00, 0x00, 0x00, 0x00  //jmp wrapout
+	};
+
+	rev_memcpy(px86.cursor, repfiniCode, sizeof(repfiniCode));
+
+	unsigned jmpLoopOffset = -1 * (jmpSize + jmpSize + loopSize); //actual code size is fixed by RelocableCodeBuffer::MarkRepFini
+	*(unsigned int *)(px86.cursor + 1) = jmpLoopOffset;
+
+	unsigned jmpWrapoutOffset = jmpSize + 3 + loopSize;
+	*(unsigned int *)(px86.cursor + 6) = jmpWrapoutOffset;
+
+	px86.cursor += sizeof(repfiniCode);
+	instrCounter+= 2;
+
+	AssembleFarloopInstruction(px86, instrCounter);
 }
 
 /* rep prefixed instructions are translated as described in RiverRepTranslator
@@ -67,16 +96,14 @@ bool RiverRepAssembler::Translate(const RiverInstruction &ri, RelocableCodeBuffe
 	switch(ri.opCode) {
 		case 0xF2:
 			px86.MarkRepInit();
-			AssembleKnownJmpInstruction(ri, px86, instrCounter);
-			AssembleLoopInstruction(ri, px86, instrCounter);
-			AssembleJmpInstruction(px86, instrCounter);
+			AssembleRepinitInstruction(ri, px86, instrCounter);
 			break;
 		case 0xF3:
-			AssembleJmpInstruction(px86, instrCounter);
+			AssembleRepfiniInstruction(px86, instrCounter);
 			px86.MarkRepFini();
 			break;
 		case 0xcc:
-			AssembleDebugBreak(ri, px86, instrCounter);
+			AssembleDebugBreak(px86, instrCounter);
 			break;
 		default:
 			DEBUG_BREAK;
