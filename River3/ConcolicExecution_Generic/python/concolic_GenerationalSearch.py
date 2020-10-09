f44f535c5df317d173f8c91a3b8afd4708526ad1
Ciprian Paduraru <ciprian.paduraru2009@gmail.com>
2020-10-09 16:55:11 +0300

    new version of concolic

--- River3/ConcolicExecution_Generic/python/concolic_GenerationalSearch.py
+++ River3/ConcolicExecution_Generic/python/concolic_GenerationalSearch.py
@@ -1,18 +1,22 @@
+# This is the implementation based on the SAGE paper
 from __future__ import print_function
 from triton     import TritonContext, ARCH, Instruction, MemoryAccess, CPUSIZE, MODE
 import  sys
 import argparse
 import concolic_utils as CU
-from typing import List, Dict
+from typing import List, Dict, Set
 import copy
 
 import logging
 logging.basicConfig(level=logging.DEBUG) # filename='example.log', # Set DEBUG or INFO if you want to see more
 
-ENTRY = None
-ENTRY_FUNC_NAME = "check" # "check" # The entry function that our emulation will start with
+ENTRY_FUNC_ADDR = None
 INPUT_BUFFER_ADDRESS = 0x10000000 # Where the input buffer will reside in the emulated program
 TARGET_TO_REACH = None  # The target instruction to reac - None by default ,check documentation
+
+# The set of basic blocks found so far. This is used to evaluate how valuate are the inputs according to the heuristic
+# Be carefully on how you use it in RL or distributed environments
+AllBlocksFound: Set[int] = set()
 
 def parseArgs():
     # Construct the argument parser
@@ -21,10 +25,10 @@ def parseArgs():
     # Add the arguments to the parser
     ap.add_argument("-bp", "--binaryPath", required=True,
                     help="where is the binary located")
+    ap.add_argument("-entryfuncName", "--entryfuncName", required=False, default="check",
+                    help="the name of the entry func you want to start the test from. By defult it is a 'check' name function!", type=str)
     ap.add_argument("-arch", "--architecture", required=True,
                     help="Architecture of the executable, one of: ARM32, ARM64, X86, X64")
-    ap.add_argument("-min", "--minLen", required=True,
-                    help="min size of input len", type=int)
     ap.add_argument("-max", "--maxLen", required=True,
                     help="max size of input len", type=int)
     ap.add_argument("-targetAddress", "--targetAddress", required=False, default=None,
@@ -50,10 +54,24 @@ def parseArgs():
     return args
 
 
-# Returns true if the optional target address was reached
-def emulate(ctx, pc):
-    targetFound = False
-    astCtxt = ctx.getAstContext()
+# Given a context where to emulate the binary already setup in memory with its input, and the PC address to emulate from, plus a few parameters...
+# Returns a tuple (true if the optional target address was reached, num new basic blocks found - if countBBlocks is True)
+def emulate(ctx, pc : int, countBBlocks : bool):
+    targetAddressFound = False
+    currentBBlockAddr = pc # The basic block address that we started to analyze currently
+    numNewBasicBlocks = 0 # The number of new basic blocks found by this function (only if countBBlocks was activated)
+    newBasicBlocksFound = set()
+
+    def onNewBasicBlockEntryFound(addr):
+        nonlocal numNewBasicBlocks
+        nonlocal newBasicBlocksFound
+        if addr not in AllBlocksFound:
+            numNewBasicBlocks += 1
+            newBasicBlocksFound.add(addr)
+            AllBlocksFound.add(addr)
+
+    onNewBasicBlockEntryFound(currentBBlockAddr)
+
     logging.info('[+] Starting emulation.')
     while pc:
         # Fetch opcode
@@ -69,35 +87,50 @@ def emulate(ctx, pc):
         logging.info(instruction)
 
         # Next
+        prevpc = pc
         pc = ctx.getConcreteRegisterValue(ctx.registers.rip)
 
+        if instruction.isControlFlow():
+            currentBBlockAddr = pc
+            onNewBasicBlockEntryFound(currentBBlockAddr)
+            logging.info(f"Instruction is control flow of type {instruction.getType()}. Addr of the new Basic block {hex(currentBBlockAddr)}")
+
         if TARGET_TO_REACH is not None and pc == TARGET_TO_REACH:
-            targetFound = True
+            targetAddressFound = True
 
     logging.info('[+] Emulation done.')
-    return targetFound
+    if countBBlocks:
+        logging.info(f'===== New basic blocks found: {[hex(intBlock) for intBlock in newBasicBlocksFound]}')
+
+    return targetAddressFound, numNewBasicBlocks
 
 # This function initializes the context memory.
-def initContext(ctx, inputToTry : CU.Input):
-    # Clean symbolic state
-    ctx.concretizeAllRegister()
-    ctx.concretizeAllMemory()
+def initContext(ctx : TritonContext, inputToTry : CU.Input, symbolized : bool):
+    assert (ctx.isSymbolicEngineEnabled() == symbolized), "Making sure that context has exactly the matching requirements for the call, nothing more, nothing less"
 
-    def symbolizeAndConcretizeByteIndex(byteIndex, value):
+    # Clean symbolic state
+    if symbolized:
+        ctx.concretizeAllRegister()
+        ctx.concretizeAllMemory()
+
+    def symbolizeAndConcretizeByteIndex(byteIndex, value, symbolized):
         byteAddr = INPUT_BUFFER_ADDRESS + byteIndex
         ctx.setConcreteMemoryValue(byteAddr, value)
-        ctx.symbolizeMemory(MemoryAccess(byteAddr, CPUSIZE.BYTE))
+
+        if symbolized:
+            ctx.symbolizeMemory(MemoryAccess(byteAddr, CPUSIZE.BYTE))
 
     # Symbolize the input bytes in the input seed.
     # Put all the inputs in the buffer in the emulated program memory
     inputLen = max(inputToTry.buffer.keys())
     for byteIndex, value in inputToTry.buffer.items():
-        symbolizeAndConcretizeByteIndex(byteIndex, value)
+        symbolizeAndConcretizeByteIndex(byteIndex, value, symbolized)
 
-    # Put the last bytes as fake sentinel inputs to promote some usages detection outside buffer
-    SENTINEL_SIZE = 4
-    for sentinelByteIndex in range(inputLen, inputLen + SENTINEL_SIZE):
-        symbolizeAndConcretizeByteIndex(sentinelByteIndex, 0)
+    if symbolized:
+        # Put the last bytes as fake sentinel inputs to promote some usages detection outside buffer
+        SENTINEL_SIZE = 4
+        for sentinelByteIndex in range(inputLen, inputLen + SENTINEL_SIZE):
+            symbolizeAndConcretizeByteIndex(sentinelByteIndex, 0, symbolized)
 
     # The commented version is the generic one if using a plain buffer and no dict
     """
@@ -116,44 +149,42 @@ def initContext(ctx, inputToTry : CU.Input):
     ctx.setConcreteRegisterValue(ctx.registers.rbp, 0x7fffffff)
     return
 
-# Load segments into triton.
-def loadBinary(ctx, path):
-    global ENTRY
 
-    logging.info(f"Loading the binary at path {path}..")
-    import lief
-    binary = lief.parse(path)
-    phdrs  = binary.segments
-    for phdr in phdrs:
-        size   = phdr.physical_size
-        vaddr  = phdr.virtual_address
-        logging.info('[+] Loading 0x%06x - 0x%06x' %(vaddr, vaddr+size))
-        ctx.setConcreteMemoryAreaValue(vaddr, phdr.content)
+# Load the binary segments into the given set of contexts
+def loadBinary(ctxList, binaryPath, entryfuncName):
+    global ENTRY_FUNC_ADDR
 
-    logging.info(f"Findind the exported function of interest {path}..")
-    res = binary.exported_functions
-    for function in res:
-        if ENTRY_FUNC_NAME in function.name:
-            ENTRY = function.address
-            logging.info(f"Function of interest found at address {ENTRY}")
-            break
+    for ctxIndex, ctx in enumerate(ctxList):
+        logging.info(f"Loading the binary at path {binaryPath}..")
+        import lief
+        binary = lief.parse(binaryPath)
+        phdrs  = binary.segments
+        for phdr in phdrs:
+            size   = phdr.physical_size
+            vaddr  = phdr.virtual_address
+            logging.info('[+] Loading 0x%06x - 0x%06x' %(vaddr, vaddr+size))
+            ctxList[ctxIndex].setConcreteMemoryAreaValue(vaddr, phdr.content)
 
-    assert ENTRY != None, "Exported function wasn't found"
+        if ENTRY_FUNC_ADDR is None:
+            logging.info(f"Findind the exported function of interest {binaryPath}..")
+            res = binary.exported_functions
+            for function in res:
+                if entryfuncName in function.name:
+                    ENTRY_FUNC_ADDR = function.address
+                    logging.info(f"Function of interest found at address {ENTRY_FUNC_ADDR}")
+                    break
+
+            assert ENTRY_FUNC_ADDR != None, "Exported function wasn't found"
 
 # This function returns a set of new inputs based on the last trace.
 def Expand(ctx, inputToTry):
     logging.info(f"Seed injected:, {inputToTry}")
 
     # Init context memory
-    initContext(ctx, inputToTry)
+    initContext(ctx, inputToTry, symbolized=True)
 
     # Emulate the binary with the setup memory
-    targetFound = emulate(ctx, ENTRY)
-
-    if targetFound:
-        logging.critical(f"The solution to get to the target address is input {inputToTry}")
-        # TODO: Bogdan put this in output somewhere , folder, visualization etc.
-        exit(0)
+    emulate(ctx, ENTRY_FUNC_ADDR, countBBlocks=False)
 
     # Set of new inputs
     inputs : List[CU.Input] = []
@@ -207,7 +238,7 @@ def Expand(ctx, inputToTry):
     # Clear the path constraints to be clean at the next execution.
     ctx.clearPathConstraints()
 
-    return targetFound, inputs
+    return inputs
 
 def SearchInputs(ctx, initialSeedDict):
 
@@ -225,17 +256,19 @@ def SearchInputs(ctx, initialSeedDict):
     while worklist:
         # Take the first seed
         inputSeed : CU.Input = worklist.extractInput()
-        targetFound, newInputs = Expand(ctx, inputSeed)
-
-        if targetFound:
-            break
+        newInputs = Expand(ctx, inputSeed)
 
         for newInp in newInputs:
             # Execute the input to detect real issues with it
             ExecuteInputToDetectIssues(newInp)
 
-            # Assign this input a priority
-            newInp.priority = ScoreHeuristic(newInp)
+            # Assign this input a priority, and check if the hacked target address was found or not
+            targetFound, newInp.priority = ScoreInput(newInp)
+
+            if targetFound:
+                logging.critical(f"The solution to get to the target address is input {newInp}")
+                # TODO: Bogdan put this in output somewhere , folder, visualization etc.
+                exit(0)
 
             # Then put it in the worklist
             worklist.addInput(newInp)
@@ -244,28 +277,40 @@ def ExecuteInputToDetectIssues(input : CU.Input):
     # TODO Bogdan, output somehow if the input has issues - folders, visual report etc...
     pass
 
-def ScoreHeuristic(newInp : CU.Input):
+def ScoreInput(newInp : CU.Input):
     # TODO Ciprian, executes the input and get using a BB based heuristics what should be its priority
-    return 100 - newInp.bound # as default, return the bound...
+    initContext(tracingContext, newInp, symbolized=False)
+    targetFound, numNewBlocks = emulate(tracingContext, ENTRY_FUNC_ADDR, countBBlocks=True)
+
+    return targetFound, numNewBlocks # as default, return the bound...
 
 if __name__ == '__main__':
 
     args = parseArgs()
 
-    ctx = TritonContext(args.architecture)
+    # Create two contexts : one symbolic used for detecting path constraints etc, and another one less heavy used only for tracing
+    symbolicContext = TritonContext(args.architecture)
+    tracingContext = TritonContext(args.architecture)
+
+    tracingContext.enableSymbolicEngine(False)
+    logging.info(f"Tracing context has enabled symboling engine ? {tracingContext.isSymbolicEngineEnabled()}")
+    logging.info(f"Symbolic context has enabled symboling engine ?{symbolicContext.isSymbolicEngineEnabled()}")
+    assert tracingContext.isSymbolicEngineEnabled() == False
+    assert symbolicContext.isSymbolicEngineEnabled() == True
 
     # Define symbolic optimizations
-    ctx.setMode(MODE.ALIGNED_MEMORY, True)
-    ctx.setMode(MODE.ONLY_ON_SYMBOLIZED, True) # ????
+    symbolicContext.setMode(MODE.ALIGNED_MEMORY, True)
+    symbolicContext.setMode(MODE.ONLY_ON_SYMBOLIZED, True) # ????
+    # TODO Ciprian: should we enable AST_OPTIMIZATIONS, CONSTANT_FOLDING
 
-    # Load the binary
-    loadBinary(ctx, args.binaryPath)
+    # Load the binary into the given set of contexts
+    loadBinary([symbolicContext, tracingContext], args.binaryPath, args.entryfuncName)
 
     # TODO: currently random input ;use corpus and dictionary like in libfuzzer
     initialSeedDict = ["good"]
     CU.processSeedDict(initialSeedDict) # Transform the initial seed dict to bytes instead of chars if needed
 
-    SearchInputs(ctx, initialSeedDict)
+    SearchInputs(symbolicContext, initialSeedDict)
 
     sys.exit(0)
 
